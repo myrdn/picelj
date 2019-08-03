6747102dd999aae9e9c85970b605ce6027deb2fe
myrdin <myrdin@protonmail.com>
2019-08-03 18:13:52 +0200

    Adding javascript toggle

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $(".grid,.grid2,.grid3,.grid4,.grid5,.grid6,.grid7,.grid8,.grid9,.grid10,.grid11").hide();
+  $("#1").click(function(){
+    $(".grid").toggle();
+  });
+ $("#2").click(function(){
+    $(".grid2,.grid3").toggle();
+  });
+  $("#3").click(function(){
+    $(".grid4,.grid5").toggle();
+  });
+  $("#4").click(function(){
+    $(".grid6,.grid7").toggle();
+  });
+  $("#5").click(function(){
+    $(".grid8,.grid9").toggle();
+  });
+  $("#6").click(function(){
+    $(".grid10,.grid11").toggle();
+  });
+});
+
